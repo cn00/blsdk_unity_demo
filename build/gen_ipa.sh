c048866d387b21c224e9d005a7c46e3148a35d71
cn <cool_navy@126.com>
2018-01-04 20:32:49 +0800

    fixed name spep 1

--- build/gen_ipa.sh
+++ build/gen_ipa.sh
@@ -1,10 +1,9 @@
 # !/us/sh
 
 set -e
-set -x
+# set -x
 
 # kill all unity
-#ps -A | gep 'Unity' | sed '/gep/d' | awk '{pint $1}' | xags -I % sudo kill -9 %
 # killall Unity
 
 
@@ -15,20 +14,20 @@ basepath=$(cd `dirname $0`;pwd)
 
 WORKSPACE=${WORKSPACE-${basepath}}
 
-archivePath="${WORKSPACE}/../ios.poj/build/Release-iphoneos/Unity-iPhone.xcarchive"
+archivePath="${WORKSPACE}/../ios.proj/build/Release-iphoneos/Unity-iPhone.xcarchive"
 exportOptionsPlist="${WORKSPACE}/../exportOptionsPlist.plist"
 
-exportPath="${WORKSPACE}/../ios.poj/build/Release-iphoneos"
+exportPath="${WORKSPACE}/../ios.proj/build/Release-iphoneos"
 
 
 OTHER_LDFLAGS="-ObjC -lresolv -weak_framework CoreMotion -weak-lSystem -Wl -undefined dynamic_lookup"
-# 6143b0ab-06d2-4c5b-9021-fe3729e0e184
 
 cd "ios.proj"
-## achive & expot
-# xcodebuild -scheme Unity-iPhone -configuration Release archive -archivePath "${archivePath}" -allowProvisioningUpdates OTHER_LDFLAGS="${OTHER_LDFLAGS}" SWIFT_VERSION=4.0 LD_RUNPATH_SEARCH_PATHS="@executable_path/Famewoks" ENABLE_BITCODE=NO DEVELOPMENT_TEAM=UL9WJVVZ42
+## achive
+xcodebuild -scheme Unity-iPhone -configuration Release archive -archivePath "${archivePath}" -allowProvisioningUpdates OTHER_LDFLAGS="${OTHER_LDFLAGS}" SWIFT_VERSION=4.0 LD_RUNPATH_SEARCH_PATHS="@executable_path/Famewoks" ENABLE_BITCODE=NO DEVELOPMENT_TEAM=UL9WJVVZ42
 
 
+## expot ipa
 xcodebuild -exportArchive -exportPath "${exportPath}" -archivePath "${archivePath}" -exportOptionsPlist "${exportOptionsPlist}" 
 
 ## upload-app
